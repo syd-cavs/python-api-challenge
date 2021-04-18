d22fe1c03ba4a0bca81442ca5bce704c526bf908
Sydney Cavallaro <sydneycavallaro@MacBook-Pro-3.local>
2021-04-18 12:50:44 -0400

    added api keys

--- WeatherPy/starter_code/api_keys.py
+++ WeatherPy/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "bd973c98262ec8af1c8c3339638bf14b"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBZga0Tern1f1f9RtglWEgd79ckDY9HdMI"
